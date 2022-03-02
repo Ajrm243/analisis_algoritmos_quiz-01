024f905a254eca5d5f9674b24ed0601589ff2c97
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2022-03-02 14:29:27 -0600

    noticiaRadio hecho

--- quiz_1.cpp
+++ quiz_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -81,11 +82,86 @@ class NoticiaPaper: Noticia {
         }
 };
 
+/*
+typedef struct mediaDitital {
+    string video;
+    string audio;
+    string foto;
+} mediaDigitalPointer;
+*/
+
+/*
+    TODO: hacer bien la noticia digital
+*/
+class NoticiaDigital: Noticia {
+    // atributos privados
+    private:
+        vector<string> listaMedia;
+    public:
+        // SETTER
+        void setListaMedia(vector<string> pListaMedia) {
+            listaMedia = pListaMedia;
+        }
+
+        // GETTER
+        string getListaMedia() {
+            string res = "\n";
+            for (string i : listaMedia) {
+                res += (i + "\n");
+            }
+            return res;
+        }
+
+        // Constructor
+        NoticiaDigital(string pTitulo, string pFecha, string pReportero, vector<string> pListaMedia) : Noticia(pTitulo, pFecha, pReportero) {
+            setListaMedia(pListaMedia);
+        }
+
+        // Devuelve el nombre de la clase
+        virtual const string classname() { return "NoticiaPaper"; }
+
+        // Método abstracto implementado
+        virtual string getInfo()  {
+            return Noticia::getInfo() + "\nMedia: " + getListaMedia();
+        }
+};
+
+class NoticiaRadio: Noticia {
+    // atributos privados
+    private:
+        string audio;
+    public:
+        // SETTER
+        void setAudio(string pAudio) {
+            audio = pAudio;
+        }
+
+        // GETTER
+        string getAudio() {
+            return audio;
+        }
+
+        // Constructor
+        NoticiaRadio(string pTitulo, string pFecha, string pReportero, string pAudio) : Noticia(pTitulo, pFecha, pReportero) {
+            setAudio(pAudio);
+        }
+
+        // Devuelve el nombre de la clase
+        virtual const string classname() { return "NoticiaRadio"; }
+
+        // Método abstracto implementado
+        virtual string getInfo()  {
+            return Noticia::getInfo() + "\nAudio: " + getAudio();
+        }
+};
+
 int main() {
 
     Noticia miNoticia = Noticia("Titulo1", "02-03-2022", "Ignacio Santos");
-    NoticiaPaper miPaper = NoticiaPaper("Titulo2", "02-06-2099", "Greyvin Moya", "./image.jpeg");
+    NoticiaPaper miPaper = NoticiaPaper("Titulo2", "02-06-2099", "Greyvin Moya", "./image02.jpeg");
+    NoticiaRadio miRadio = NoticiaRadio("Titulo3", "24-03-2050", "Andres Vargas", "./audio03.flac");
     //cout << miNoticia.getInfo() << endl;
-    cout << miPaper.getInfo() << endl;
+    //cout << miPaper.getInfo() << endl;
+    cout << miRadio.getInfo() << endl;
     return 0;
 }
